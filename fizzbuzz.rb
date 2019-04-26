1dca58cebb97059b84d35342abe45ff8c3054a71
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-26 17:01:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(x)
-  if x % == 3
-    puts "Fizz"
-  elsif x % == 5
-    puts "Buzz"
-  elsif x % == 15
+  if x % 3 == 0 && x % 5 == 0
     puts "FizzBuzz"
-  else x % == 4
+  elsif x % 3 == 0
+    puts "Fizz"
+  elsif x % 5 == 0
+    puts "Buzz"
+  else 
     puts nil
   end
 end
