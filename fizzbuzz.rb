1b5b06a417c0551d211376cc042837c3abcf2e46
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-26 16:50:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
+def fizzbuzz(x)
+  if x == fizzbuzz(3)
+    puts "Fizz"
+  elsif x == fizzbuzz(5)
+    puts "Buzz"
+  elsif x == fizzbuzz(15)
+    puts "FizzBuzz"
+  else x == fizzbuzz(4)
+    puts nil
+end
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
